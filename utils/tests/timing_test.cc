c6ca4b773fd15d736352b0a615898c986b2cbb3f
Alvin Sun <alvinsunyixiao@gmail.com>
2019-11-03 16:31:52 -0800

    tic toc working

--- utils/tests/timing_test.cc
+++ utils/tests/timing_test.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "utils/timing/tic_toc.h"
 #include "utils/gtest_utils/test_base.h"
 
@@ -6,10 +8,69 @@ using namespace timing;
 class TimingTest : public TestBase {
  public:
   void TicTocTest() {
-    TicToc tictoc("Raw TicToc");
-    for (double i = 0; i < 100000; ++i);
-    tictoc.Toc();
+    TicTocGlobalReset();
+    TicToc tictoc_out("Outer Loop");
+    TicToc tictoc_in("Inner Loop");
+    // first run
+    tictoc_out.Tic();
+    for (double i = 0; i < 1000; ++i) {
+      tictoc_in.Tic();
+      for (double j = 0; j < 1000; ++j);
+      tictoc_in.Toc();
+    }
+    tictoc_out.Toc();
+    // second run
+    tictoc_out.Tic();
+    for (double i = 0; i < 1000; ++i) {
+      tictoc_in.Tic();
+      for (double j = 0; j < 1000; ++j);
+      tictoc_in.Toc();
+    }
+    tictoc_out.Toc();
+    // third run
+    tictoc_out.Tic();
+    for (double i = 0; i < 1000; ++i) {
+      tictoc_in.Tic();
+      for (double j = 0; j < 1000; ++j);
+      tictoc_in.Toc();
+    }
+    tictoc_out.Toc();
+    std::cout << "[ TicTocTest ] Time Summary:" << std::endl;
+    std::cout << TicTocGlobalSummary() << std::endl;
+  }
+
+  void TicTocScopeTest() {
+    TicTocGlobalReset();
+
+    {
+      TIC_TOC_SCOPE(TicTocScopeTest);
+      for (double i = 0; i < 1000000; ++i);
+    }
+
+    std::cout << "[ TicTocScopeTest ] Time Summary:" << std::endl;
+    std::cout << TicTocGlobalSummary() << std::endl;
+  }
+
+  void TicTocFunctionTest() {
+    TicTocGlobalReset();
+
+    TimeConsumingFunction();
+    TimeConsumingFunction();
+    TimeConsumingFunction();
+    TimeConsumingFunction();
+    TimeConsumingFunction();
+
+    std::cout << "[ TicTocFunctionTest ] Time Summary:" << std::endl;
+    std::cout << TicTocGlobalSummary() << std::endl;
+  }
+
+ private:
+  void TimeConsumingFunction() {
+    TIC_TOC_FUNCTION();
+    for (double i = 0; i < 1000000; ++i);
   }
 };
 
 TEST_FM(TimingTest, TicTocTest)
+TEST_FM(TimingTest, TicTocScopeTest)
+TEST_FM(TimingTest, TicTocFunctionTest)
--- utils/timing/tic_toc.h
+++ utils/timing/tic_toc.h
@@ -2,8 +2,15 @@
 
 #include <chrono>
 #include <string>
+#include <unordered_map>
+
+#define kSecPerNanoSec 1e-9
 
 namespace timing {
+
+/***********************
+ * --- TicTocStats --- *
+ ***********************/
 
 class TicTocStats {
  public:
@@ -25,13 +32,25 @@ class TicTocStats {
   int64_t total_time_ns_;
 };
 
+/**********************
+ * --- TicTocBank --- *
+ **********************/
+
 class TicTocBank {
  public:
   TicTocBank();
 
   void Reset();
   void Update(const std::string &name, int64_t duration_ns);
+  std::unordered_map<std::string, TicTocStats> GetSummary();
+ 
+ private:
+  std::unordered_map<std::string, TicTocStats> channel_map_;
 };
+
+/******************
+ * --- TicToc --- *
+ ******************/
 
 class TicToc {
  public:
@@ -44,5 +63,26 @@ class TicToc {
   std::chrono::time_point<std::chrono::high_resolution_clock> start_;
   std::string name_;
 };
+
+class ScopedTicToc {
+ public:
+  explicit ScopedTicToc(const std::string &name);
+  ~ScopedTicToc();
+
+ private:
+  std::chrono::time_point<std::chrono::high_resolution_clock> start_;
+  std::string name_;
+};
+
+#define TIC_TOC_SCOPE(name) ScopedTicToc scoped_tictoc_##name(#name)
+#define TIC_TOC_FUNCTION() ScopedTicToc function_scoped_tictoc(__FUNCTION__)
+
+/**************************
+ * --- Global Context --- *
+ **************************/
+
+void TicTocGlobalUpdate(const std::string &name, int64_t duration_ns);
+void TicTocGlobalReset();
+std::string TicTocGlobalSummary();
 
 } // namespace timing
