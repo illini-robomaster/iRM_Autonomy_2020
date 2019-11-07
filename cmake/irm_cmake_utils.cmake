1ccf00a0d9ff155bb14a14009eceb0c371fc979c
alvin <alvinsunyixiao@gmail.com>
2019-11-06 20:20:50 -0800

    move test binary to tests dir; fix timing namespace

--- cmake/irm_cmake_utils.cmake
+++ cmake/irm_cmake_utils.cmake
@@ -5,6 +5,8 @@
 function(irm_add_cc_test)
     cmake_parse_arguments(TEST_ARG "" "NAME" "DEPENDS" ${ARGN})
     add_executable(${TEST_ARG_NAME} tests/${TEST_ARG_NAME}.cc)    
+    set_target_properties(${TEST_ARG_NAME} PROPERTIES
+        RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests)
     target_link_libraries(${TEST_ARG_NAME} ${TEST_ARG_DEPENDS})
     gtest_discover_tests(${TEST_ARG_NAME})
 endfunction()
